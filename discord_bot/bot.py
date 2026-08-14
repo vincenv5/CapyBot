4d1e1343d58c71d0c2ff3681595b342f5b1db4ee
Vincent Vu <vincenv5@uci.edu>
2026-08-14 13:34:53 -0700

    Added joke command and doc
    The bot can now send jokes over the chat when the user types in '!joke'. Docstrings and commands were also added for some clarity.

--- discord_bot/bot.py
+++ discord_bot/bot.py
@@ -3,7 +3,10 @@ import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+from joke import get_full_joke
 
+
+# setting up global constants, file, intents, and environment variables
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 handler = logging.FileHandler(filename='discord_bot\\bot.log', encoding='utf-8', mode='w')
@@ -11,11 +14,16 @@ intents = discord.Intents.default()
 intents.message_content = True
 comm_prefix = '!'
 
+
+# This establishes the bot with the command prefix, intents, log, and logging level
 client = commands.Bot(command_prefix=comm_prefix, intents=intents, log_handler=handler, log_level=logging.DEBUG)
 
+
+"""The client.events are what the bot will look out for."""
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
+
 
 @client.event
 async def on_message(message):
@@ -28,9 +36,16 @@ async def on_message(message):
     await client.process_commands(message)
 
 
+"""The commands will run once it detects when the user sends a message that includes the command prefix
+   followed by the command phrase (i.e the name of the functions that are described below)"""
 @client.command()
-async def weather(ctx, *msg):
-    await ctx.send(f"weather {msg}")
+async def joke(ctx):
+    await ctx.send(get_full_joke())
+
+
+@client.command()
+async def guide(ctx):
+    return 0
 
 
 if __name__ == "__main__":
